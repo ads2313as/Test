1ea9560c30804482b35b55bc7c3bf7f326d51ae5
ads2313as <ads2313as@tlen.pl>
2020-07-08 15:19:44 +0200

    Correction

--- Main.py
+++ Main.py
@@ -1,3 +1,11 @@
-import Initialize
+import Initialize, AuxiliaryFunctions
 
-print('xd')
+black_win = True
+white_win = False
+draw = False
+
+white_turn = True
+black_turn = not white_turn
+while not (white_win or black_win or draw):
+    move = input()
+print(AuxiliaryFunctions.algebraic_notation_to_tuple("A5"))
