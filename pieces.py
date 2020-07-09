090bc521f55af93daa42566122220a51dea61bb0
terec <terechsama@gmail.com>
2020-07-09 12:23:34 +0200

    Bishop moves.

--- pieces.py
+++ pieces.py
@@ -48,9 +48,46 @@ class Pawn(Piece):
             if b_utils.is_inside_board(attack): attacks.append(attack)
         return attacks
 
+class Bishop(Piece):
+    def possibleMoves(self):
+        moves = []
+        x, y = self.x+1, self.y+1
+        while b_utils.is_inside_board((x,y)):
+            moves.append((x,y))
+            x += 1
+            y += 1
+        x, y = self.x + 1, self.y - 1
+        while b_utils.is_inside_board((x, y)):
+            moves.append((x, y))
+            x += 1
+            y -= 1
+        x, y = self.x - 1, self.y - 1
+        while b_utils.is_inside_board((x, y)):
+            moves.append((x, y))
+            x -= 1
+            y -= 1
+        x, y = self.x - 1, self.y + 1
+        while b_utils.is_inside_board((x, y)):
+            moves.append((x, y))
+            x -= 1
+            y += 1
+        return moves
+
+    def possibleAttacks(self):
+        return self.possibleMoves()
+
 
 #Testing
 if __name__ == "__main__":
+    print("Testing PAWNS")
     pawn = Pawn(3, 3, "B")
     print(pawn.possibleAttacks())
     print(pawn.possibleMoves())
+
+    print("Testing Bishop")
+    bishop = Bishop(0,0, "B")
+    print(bishop.possibleMoves())
+    bishop = Bishop(7, 7, "B")
+    print(bishop.possibleMoves())
+    bishop = Bishop(3, 3, "B")
+    print(bishop.possibleMoves()) # should be equal to [(4, 4), (5, 5), (6, 6), (7, 7), (4, 2), (5, 1), (6, 0), (2, 2), (1, 1), (0, 0), (2, 4), (1, 5), (0, 6)]
